a80288165fd07a3f0f87247b5a5a1d35cf2dcb37
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-13 11:23:01 -0400

    Done

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const currentUser = 'Petey the Pedophilic Turtle';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
-// comment
