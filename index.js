a7bcf8a0d320aca7ee5d8e329f6cdb4fdef8d72f
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-13 11:16:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Petey the Pedophilic Turtle';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
